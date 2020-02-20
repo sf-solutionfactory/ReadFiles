127a4d8d02cce55222cb8fdf95c0d564f5f46d38
Omar <pinkyz.90@gmail.com>
2020-02-20 17:20:33 -0600

    Ingresar tarea en el programador

--- ReadFiles/SharedSettings/AppSettings.cs
+++ ReadFiles/SharedSettings/AppSettings.cs
@@ -148,6 +148,8 @@ namespace SharedSettings
         public bool esActivoNomI
         { get; set; }
 
+        public bool esActivoWindows
+        { get; set; }
         public Settings()
         {
             sap_client = "";
@@ -184,6 +186,8 @@ namespace SharedSettings
             prox_pass = "";
             prox_puert = 0;
             prox_user = "";
+
+            esActivoWindows = false;
         }
     }
 }
